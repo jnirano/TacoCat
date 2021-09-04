669c07d80138c9dc7c00fb77142066daa036fa09
Joel Ogunniran <joelnirano@gmail.com>
2021-09-04 22:18:02 +0100

    Adding palindrome codes
    Adding tacocat.js

--- js/tacocat.js
+++ js/tacocat.js
@@ -1,4 +1,54 @@
-// Call Hello World
-function helloWorld(){
-   alert("hello World");
+// Get our user input from the page/html
+function getValue(){
+
+   //Make sure the alert is visible
+   document.getElementById("alert").classList.add("invisible");
+   
+   //get user string for the page
+   let userString = document.getElementById("userString").value;
+
+   //Check for a palindrome
+   let returnObj = checkForPalindrome(userString);
+
+   //Display out message to the screen
+   displayMessage(returnObj);
+}
+
+//Check if string is a palindrome
+function checkForPalindrome(userString){
+   //Convert string to lowercase
+   userString = userString.toLowerCase();
+
+   //Remove spaces and special characters: Using Regex
+   let regex = /[^a-z0-9]/gi;
+   userString = userString.replace(regex,"");
+
+   //Reverse the string
+   let revString = []; //array
+   let returnObj = {}; //Oject
+
+  for (let index = userString.length - 1; index >= 0; index--) {
+     revString += userString[index];  
+  }
+
+  //Check if userString === revString
+  if (revString == userString) {
+     returnObj.msg = "Congratulations !! You successfully entered a palindrome!"
+  } 
+  else {
+     returnObj.msg = "Sorry!!! You did not entered a palindrome."
+  }
+  returnObj.reverse = userString;
+
+  return returnObj;
+
+}
+
+//Display a message to the screen
+function displayMessage() {
+   
+   document.getElementById("alertHeader").innerHTML = returnObj.msg;
+   document.getElementById("msg").innerHTML = "Your phrase reverse is: ${returnObj.reverse}";
+   document.getElementById("alert").classList.remove("invisible");
+
 }
